cc7c677afc0e94616fef65ad0805c07bcc31cfb1
aziz <dulaziz268@gmail.com>
2026-07-01 22:54:08 +0700

    fix: access path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            $appUrl = (string) config('app.url');
+            $appUrl = rtrim((string) config('app.url'), '/');
+
+            // Shared hosting users often set APP_URL with /public.
+            // Normalize it so generated URLs always target domain root.
+            if (str_ends_with($appUrl, '/public')) {
+                $appUrl = substr($appUrl, 0, -7);
+            }
 
             if ($appUrl !== '') {
                 URL::forceRootUrl($appUrl);
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,9 +9,9 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&family=Cormorant+Garamond:wght@500;600;700;800&display=swap" rel="stylesheet">
-  <link href={{ asset("template/assets/css/nucleo-icons.css")}} rel="stylesheet" />
-  <link href={{ asset("template/assets/css/nucleo-svg.css")}} rel="stylesheet" />
-  <link id="pagestyle" href={{ asset("template/assets/css/argon-dashboard.css?v=2.1.0")}} rel="stylesheet" />
+  <link href="{{ asset('template/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{ asset('template/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <link id="pagestyle" href="{{ asset('template/assets/css/argon-dashboard.css') }}?v=2.1.0" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="{{ asset('css/batik.css') }}">
   @stack('styles')
@@ -94,11 +94,11 @@
     </div>
   </footer>
 
-  <script src={{ asset("template/assets/js/core/popper.min.js")}}></script>
-  <script src={{ asset("template/assets/js/core/bootstrap.min.js")}}></script>
-  <script src={{ asset("template/assets/js/plugins/perfect-scrollbar.min.js")}}></script>
-  <script src={{ asset("template/assets/js/plugins/smooth-scrollbar.min.js")}}></script>
-  <script src={{ asset("template/assets/js/argon-dashboard.min.js?v=2.1.0")}}></script>
+  <script src="{{ asset('template/assets/js/core/popper.min.js') }}"></script>
+  <script src="{{ asset('template/assets/js/core/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('template/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+  <script src="{{ asset('template/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+  <script src="{{ asset('template/assets/js/argon-dashboard.min.js') }}?v=2.1.0"></script>
   <script src="{{ asset('js/batik.js') }}"></script>
   @stack('scripts')
 </body>
