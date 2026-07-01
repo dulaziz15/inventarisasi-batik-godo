e6ac24a6a826046b429b216ae08d81430a8b2e0d
aziz <dulaziz268@gmail.com>
2026-07-01 22:41:33 +0700

    fix: struktur

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,20 +1,24 @@
 <head>
+  @php
+    $basePath = rtrim(request()->getBaseUrl(), '/');
+    $assetBase = $basePath === '' ? '' : $basePath;
+  @endphp
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="{{ url('template/assets/img/apple-icon.png') }}">
-  <link rel="icon" type="image/png" href="{{ url('template/assets/img/favicon.png') }}">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ $assetBase }}/template/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="{{ $assetBase }}/template/assets/img/favicon.png">
   <title>{{ trim($__env->yieldContent('title', 'Inventarisasi Batik Godo')) }}</title>
   <!--     Fonts and icons     -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@500;600;700;800&family=Manrope:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="{{ url('template/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-  <link href="{{ url('template/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <link href="{{ $assetBase }}/template/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="{{ $assetBase }}/template/assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <!-- CSS Files -->
-  <link id="pagestyle" href="{{ url('template/assets/css/argon-dashboard.css?v=2.1.0') }}" rel="stylesheet" />
-  <link rel="stylesheet" href="{{ url('css/batik.css') }}">
+  <link id="pagestyle" href="{{ $assetBase }}/template/assets/css/argon-dashboard.css?v=2.1.0" rel="stylesheet" />
+  <link rel="stylesheet" href="{{ $assetBase }}/css/batik.css">
 </head>
--- resources/views/components/script.blade.php
+++ resources/views/components/script.blade.php
@@ -1,9 +1,13 @@
+  @php
+    $basePath = rtrim(request()->getBaseUrl(), '/');
+    $assetBase = $basePath === '' ? '' : $basePath;
+  @endphp
 <!--   Core JS Files   -->
-  <script src="{{ url('template/assets/js/core/popper.min.js') }}"></script>
-  <script src="{{ url('template/assets/js/core/bootstrap.min.js') }}"></script>
-  <script src="{{ url('template/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
-  <script src="{{ url('template/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
-  <script src="{{ url('template/assets/js/plugins/chartjs.min.js') }}"></script>
+  <script src="{{ $assetBase }}/template/assets/js/core/popper.min.js"></script>
+  <script src="{{ $assetBase }}/template/assets/js/core/bootstrap.min.js"></script>
+  <script src="{{ $assetBase }}/template/assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="{{ $assetBase }}/template/assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="{{ $assetBase }}/template/assets/js/plugins/chartjs.min.js"></script>
   <script>
     var chartCanvas = document.getElementById("chart-line");
 
@@ -101,6 +105,6 @@
     }
   </script>
   <!-- Github buttons -->
-  <script async defer src="{{ url('template/assets/js/buttons.js') }}"></script>
+  <script async defer src="{{ $assetBase }}/template/assets/js/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="{{ url('template/assets/js/argon-dashboard.min.js?v=2.1.0') }}"></script>
+  <script src="{{ $assetBase }}/template/assets/js/argon-dashboard.min.js?v=2.1.0"></script>
